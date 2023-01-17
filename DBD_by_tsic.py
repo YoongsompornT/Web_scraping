263d8a253df946a73412528a56d6a907db08ea4d
YoongsompornT <yoongsomporn.t@gmail.com>
2023-01-17 13:08:57 +0900

    Update DBD_by_tsic.py

--- DBD_by_tsic.py
+++ DBD_by_tsic.py
@@ -1,12 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
 import time
